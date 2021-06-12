0b176065ac4f291bdb9da4bce544e93ff2ca9901
Марат Халилуллин <resfari@MacBook-Pro-Marat.local>
2021-06-12 18:03:43 +0300

    add getEventDescription method

--- cmd/test-api/main.go
+++ cmd/test-api/main.go
@@ -5,18 +5,52 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/valyala/fasthttp"
+	"sync"
 	"time"
 )
 
+type MutexEvents struct {
+	sync.Mutex
+	Events []Event
+}
+
+
+
 func main() {
-	loc := "msk"
-	cat := []string{"concert", "cinema", "exhibition"}
+	loc := "kzn"
+	cat := []string{"concert"}
 	timeNow := time.Now()
 	timeNowUnix := timeNow.Unix()
-	timeAfterUnix := timeNow.Add(time.Hour * 24 * 7).Unix()
+	timeAfterUnix := timeNow.Add(time.Hour * 24 * 20).Unix()
 	params := New(loc, cat, timeNowUnix, timeAfterUnix, false)
 
-	GetEvents(params)
+	mtx := MutexEvents{}
+	events, err := GetEvents(params)
+	if err == nil && len(events.Results) != 0 {
+		fmt.Println("найдено эвентов:", len(events.Results))
+		wg := sync.WaitGroup{}
+
+		for _, event := range events.Results {
+			if event.Id == nil {
+				continue
+			}
+
+			wg.Add(1)
+			go func(id int) {
+				defer wg.Done()
+				event, err := GetEventDescription(id)
+				if err == nil && event != nil {
+					mtx.Lock()
+					defer mtx.Unlock()
+
+					mtx.Events = append(mtx.Events, *event)
+				}
+			}(*event.Id)
+		}
+		wg.Wait()
+	}
+
+	fmt.Println("events finded:", len(mtx.Events))
 }
 
 const (
@@ -59,8 +93,8 @@ func GetEvents(params Params) (*EventsResp, error) {
 		return nil, err
 	}
 
-	body := resp.Body()
-	fmt.Printf("Response body is: %s", body)
+	//body := resp.Body()
+	//fmt.Printf("Response body is: %s", body)
 
 	var events EventsResp
 
@@ -70,11 +104,50 @@ func GetEvents(params Params) (*EventsResp, error) {
 	}
 
 	//Debug
-	js, _ := json.MarshalIndent(events, "", "    ")
-	fmt.Println(string(js))
+	//js, _ := json.MarshalIndent(events, "", "    ")
+	//fmt.Println(string(js))
 	return &events, nil
 }
 
-func GetEventDescription(id int64) {
+func GetEventDescription(id int) (*Event, error) {
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
 
+	url := fmt.Sprintf("%s%d/", kudaGoUrl, id)
+
+	//debug
+	fmt.Println("url:", url)
+
+	req.SetRequestURI(url)
+
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+
+	err := fasthttp.Do(req, resp)
+	if err != nil {
+		fmt.Printf("Client get failed: %s\n", err)
+		return nil, err
+	}
+	if resp.StatusCode() != fasthttp.StatusOK {
+		fmt.Printf("Expected status code %d but got %d\n", fasthttp.StatusOK, resp.StatusCode())
+		return nil, err
+	}
+
+	contentType := resp.Header.Peek("Content-Type")
+	if bytes.Index(contentType, []byte("application/json")) != 0 {
+		fmt.Printf("Expected content type application/json but got %s\n", contentType)
+		return nil, err
+	}
+
+	var event Event
+
+	if err := json.Unmarshal(resp.Body(), &event); err != nil {
+		fmt.Printf("Unmarshal err: %s\n", err.Error())
+		return nil, err
+	}
+
+	//Debug
+	//js, _ := json.MarshalIndent(event, "", "    ")
+	//fmt.Println(string(js))
+	return &event, nil
 }
--- cmd/test-api/models.go
+++ cmd/test-api/models.go
@@ -36,3 +36,73 @@ type EventShort struct {
 	Title *string `json:"title"`
 	Slug  *string `json:"slug"`
 }
+
+type Event struct {
+	ID              int          `json:"id"`
+	PublicationDate int          `json:"publication_date"`
+	//Dates           Dates        `json:"dates"`
+	//Title           string       `json:"title"`
+	//Slug            string       `json:"slug"`
+	//Place           Place        `json:"place"`
+	//Description     string       `json:"description"`
+	//BodyText        string       `json:"body_text"`
+	//Location        Location     `json:"location"`
+	//Categories      []string     `json:"categories"`
+	//Tagline         string       `json:"tagline"`
+	//AgeRestriction  string       `json:"age_restriction"`
+	//Price           string       `json:"price"`
+	//IsFree          bool         `json:"is_free"`
+	//Images          Images       `json:"images"`
+	//FavoritesCount  int          `json:"favorites_count"`
+	//CommentsCount   int          `json:"comments_count"`
+	//SiteURL         string       `json:"site_url"`
+	//ShortTitle      string       `json:"short_title"`
+	//Tags            []string     `json:"tags"`
+	//DisableComments bool         `json:"disable_comments"`
+	//Participants    Participants `json:"participants"`
+}
+
+type Dates []Date
+
+type Date struct {
+	Start int `json:"start"`
+	End   int `json:"end"`
+}
+
+type Place struct {
+	ID int `json:"id"`
+}
+
+type Location struct {
+	Slug string `json:"slug"`
+}
+
+type Images []Image
+
+type Image struct {
+	Image  string `json:"image"`
+	Source struct {
+		Link string `json:"link"`
+		Name string `json:"name"`
+	} `json:"source"`
+}
+
+type Participants []Participant
+
+type Participant struct {
+	Role  Role  `json:"role"`
+	Agent Agent `json:"agent"`
+}
+
+type Role struct {
+	Slug string `json:"slug"`
+}
+
+type Agent struct {
+	ID        int           `json:"id"`
+	Title     string        `json:"title"`
+	Slug      string        `json:"slug"`
+	AgentType string        `json:"agent_type"`
+	Images    []interface{} `json:"images"` //думаю это все не надо вообще
+	SiteURL   string        `json:"site_url"`
+}
